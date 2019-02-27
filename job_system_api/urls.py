40fccb3d77a956daebbda2e638622d5b00dd187a
Dario Vischi <dario.vischi@fmi.ch>
2019-02-27 14:51:47 +0100

    Renamed url parameters and added ETag validator for conditional requests on job resources

--- job_system_api/urls.py
+++ job_system_api/urls.py
@@ -7,16 +7,18 @@ from job_system_api.views import JobParameterListView, JobParameterDetailsView
 from job_system_api.views import JobLogEntryListView, JobLogEntryDetailsView
 
 
+# Note: DRF expects the model field to be named 'pk'
+# @see: https://www.django-rest-framework.org/api-guide/generic-views/
 app_name = "job_system_api"
 urlpatterns = [
     url(r'^jobtemplates/$', JobTemplateListView.as_view(), name="jobtemplate_list"),
-    url(r'^jobtemplates/(?P<pk>[0-9]+)/$', JobTemplateDetailsView.as_view(), name="jobtemplate_details"),
+    url(r'^jobtemplates/(?P<templ_id>[0-9]+)/$', JobTemplateDetailsView.as_view(), name="jobtemplate_details"),
     url(r'^jobs/$', JobListView.as_view(), name="job_list"),
-    url(r'^jobs/(?P<pk>[0-9]+)/$', JobDetailsView.as_view(), name="job_details"),
+    url(r'^jobs/(?P<job_id>[0-9]+)/$', JobDetailsView.as_view(), name="job_details"),
     url(r'^jobs/(?P<job_id>[0-9]+)/parameters/$', JobParameterListView.as_view(), name="job_parameter_list"),
-    url(r'^jobs/(?P<job_id>[0-9]+)/parameters/(?P<pk>[0-9]+)/$', JobParameterDetailsView.as_view(), name="job_parameter_details"),
+    url(r'^jobs/(?P<job_id>[0-9]+)/parameters/(?P<parameter_id>[0-9]+)/$', JobParameterDetailsView.as_view(), name="job_parameter_details"),
     url(r'^jobs/(?P<job_id>[0-9]+)/logs/$', JobLogEntryListView.as_view(), name="job_log_list"),
-    url(r'^jobs/(?P<job_id>[0-9]+)/logs/(?P<pk>[0-9]+)/$', JobLogEntryDetailsView.as_view(), name="job_log_details"),
+    url(r'^jobs/(?P<job_id>[0-9]+)/logs/(?P<log_id>[0-9]+)/$', JobLogEntryDetailsView.as_view(), name="job_log_details"),
     # url(r'^jobs/(?P<pk>[0-9]+)/update$', JobCreateView.as_view(), name='jobs_create'),
 ]
 
--- job_system_api/views.py
+++ job_system_api/views.py
@@ -1,8 +1,9 @@
 from datetime import timedelta
-from django_filters import rest_framework as filters
+from django_filters import rest_framework
 from django.db.models import Q
 from django.utils import timezone
-from rest_framework import generics
+from django.views.decorators.http import etag, condition
+from rest_framework import generics, filters
 
 from job_system_api.models import JobTemplate, Job, JobParameter, JobLogEntry
 from job_system_api.models import JOB_TYPE_CHOICES
@@ -22,24 +23,30 @@ class JobTemplateListView(generics.ListCreateAPIView):
 
 class JobTemplateDetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
+    # @see rest_framework/generics.py, line 77
+    lookup_url_kwarg = 'templ_id'  # capturing group of the url pattern
+    lookup_field = 'id'  # primary key of the model
+
     queryset = JobTemplate.objects.all()
     serializer_class = JobTemplateSerializer
 
 
-class JobFilter(filters.FilterSet):
-    type_name = filters.CharFilter(label="Type Name", method="filter_type")
-    is_archived = filters.BooleanFilter(
-        label="Is Archived", method="filter_archived"
+class JobFilter(rest_framework.FilterSet):
+    type_name = rest_framework.CharFilter(
+        label="Type name", method="filter_type_name"
+    )
+    days_since_creation = rest_framework.NumberFilter(
+        label="Days since creation", method="filter_days_since_creation"
     )
 
     class Meta:
         model = Job
         fields = [
             'id', 'namespace', 'name', 'type_name', 'type', 'status',
-            'progress', 'owner', 'date_created'
+            'progress', 'owner', 'days_since_creation'
         ]
 
-    def filter_type(self, queryset, name, value):
+    def filter_type_name(self, queryset, name, value):
         type = [
             choice_key for choice_key, choice_value in JOB_TYPE_CHOICES
             if choice_value == value
@@ -47,17 +54,13 @@ class JobFilter(filters.FilterSet):
         return queryset.filter(
             type=type[0] if len(type) == 1 else -1
         )
-    
-    def filter_archived(self, queryset, name, value):
-        if value == True:
-            return queryset.filter(
-                ~Q(status='in progress') &
-                Q(date_modified__date__lt=timezone.now()-timedelta(days=7))
-            )
+
+    def filter_days_since_creation(self, queryset, name, value):
+        if value is None or value != int(value):  # check if value is an integer
+            return queryset
         else:
             return queryset.filter(
-                Q(status='in progress') |
-                Q(date_modified__date__gte=timezone.now()-timedelta(days=7))
+                Q(date_modified__date__gte=timezone.now()-timedelta(days=int(value)))
             )
 
 
@@ -65,9 +68,11 @@ class JobListView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = Job.objects.all()
     serializer_class = JobSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (rest_framework.DjangoFilterBackend, filters.OrderingFilter)
     filter_class = JobFilter
-    filter_fields = ('type', 'status', 'date_created')
+    filter_fields = ('type', 'status')
+    ordering = ('-date_created',)
+    ordering_fields = ('type', 'status', 'date_created')
 
     def perform_create(self, serializer):
         """Save the post data when creating a new job."""
@@ -76,8 +81,50 @@ class JobListView(generics.ListCreateAPIView):
 
 class JobDetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
+    # @see rest_framework/generics.py, line 77
+    lookup_url_kwarg = 'job_id'  # capturing group of the url pattern
+    lookup_field = 'id'  # primary key of the model
+
     queryset = Job.objects.all()
     serializer_class = JobDetailsSerializer
+
+    def get(self, request, *args, **kwargs):
+        etag_func = (
+            lambda request, *args, **kwargs:
+                str(hash(Job.objects.get(pk=kwargs['job_id'])))
+        )
+        last_modified_func = (
+            lambda request, *args, **kwargs:
+                Job.objects.get(pk=kwargs['job_id']).date_modified
+        )
+        @condition(
+            etag_func=etag_func,
+            last_modified_func=last_modified_func
+        )
+        def _get(request, *args, **kwargs):
+            return generics.RetrieveUpdateDestroyAPIView.get(
+                self, request, *args, **kwargs
+            )
+        return _get(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        etag_func = (
+            lambda request, *args, **kwargs:
+                str(hash(Job.objects.get(pk=kwargs['job_id'])))
+        )
+        last_modified_func = (
+            lambda request, *args, **kwargs:
+                Job.objects.get(pk=kwargs['job_id']).date_modified
+        )
+        @condition(
+            etag_func=etag_func,
+            last_modified_func=last_modified_func
+        )
+        def _put(request, *args, **kwargs):
+            return generics.RetrieveUpdateDestroyAPIView.put(
+                self, request, *args, **kwargs
+            )
+        return _put(request, *args, **kwargs)
 
 
 class JobParameterListView(generics.ListCreateAPIView):
@@ -96,8 +143,16 @@ class JobParameterListView(generics.ListCreateAPIView):
 
 class JobParameterDetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
-    queryset = JobParameter.objects.all()
+    # @see rest_framework/generics.py, line 77
+    lookup_url_kwarg = 'parameter_id'  # capturing group of the url pattern
+    lookup_field = 'id'  # primary key of the model
+
     serializer_class = JobParameterSerializer
+
+    def get_queryset(self):
+        return JobParameter.objects.filter(
+            job=self.kwargs['job_id']
+        )
 
     def perform_update(self, serializer):
         serializer.save(job=Job.objects.get(pk=self.kwargs['job_id']))
@@ -119,8 +174,16 @@ class JobLogEntryListView(generics.ListCreateAPIView):
 
 class JobLogEntryDetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
-    queryset = JobLogEntry.objects.all()
+    # @see rest_framework/generics.py, line 77
+    lookup_url_kwarg = 'log_id'  # capturing group of the url pattern
+    lookup_field = 'id'  # primary key of the model
+
     serializer_class = JobLogEntrySerializer
+
+    def get_queryset(self):
+        return JobLogEntry.objects.filter(
+            job=self.kwargs['job_id']
+        )
 
     def perform_update(self, serializer):
         serializer.save(job=Job.objects.get(pk=self.kwargs['job_id']))
